07e74ac8542d3a1dbaf8a349bd5c6ddee112e387
Marcus Williams <tue79528@temple.edu>
2017-12-04 19:29:47 -0500

    first commit

--- iCitizen/RootViewController.swift
+++ iCitizen/RootViewController.swift
@@ -12,7 +12,7 @@ class RootViewController: UIViewController, UIPageViewControllerDelegate {
 
     var pageViewController: UIPageViewController?
 
-
+    //test test test
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
