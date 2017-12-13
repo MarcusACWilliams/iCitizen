22654d8030f68c3f24e3b73a5aeaad45c826c4be
Marcus Williams <tue79528@tech-b013.tu.temple.edu>
2017-12-13 17:56:42 -0500

    fixed text

--- iCitizen/SecondViewController.swift
+++ iCitizen/SecondViewController.swift
@@ -33,17 +33,19 @@ class SecondViewController: UIViewController {
         
         stepsun = ["First things first. Don't Panic! Routine traffic stops are nothing to be afraid of. Just remeber to be polite and cooperative and everything will go smoothly.\n\n(1)First make sure you are parked in a safe well lit area.","Now that you're in a safe area let's get ready for he officer.\n(1) Roll Down your diver side window and turn on your cabin light above your head\n(2) In most States you will need the following:\n\tYour Drivers Liscencse\n\tYour Auto Insurance card.\n*Note If your insurance company issues digital cards you can bring it up on your cellphone\nYour Vehicle Regristration *Note You may not have a registraion card. You might have a sticker on the lower left corner of your windshield" ]
         
-        var i = 1
-        while (i <= stepsun.count){
+        var i = 0
+        while (i < stepsun.count){
         
-        myMutableString = NSMutableAttributedString(string: stepsun[1], attributes: [NSAttributedStringKey.font:UIFont(name: "Georgia", size: 20.0)!])
+        myMutableString = NSMutableAttributedString(string: stepsun[i], attributes: [NSAttributedStringKey.font:UIFont(name: "Georgia", size: 20.0)!])
         myMutableString.addAttribute(NSAttributedStringKey.foregroundColor, value: UIColor.red, range: NSRange(location:2,length:4))
         
-        steps[i] = myMutableString
+        steps.append(myMutableString)
+            
+            i += 1
         }
         questions = ["Are you in a safe Area?"]
         
-         stepLable.attributedText = steps[1]
+         stepLable.attributedText = steps[0]
          stepLable.sizeToFit()
     }
     
