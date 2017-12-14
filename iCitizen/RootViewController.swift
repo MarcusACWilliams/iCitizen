5d4e400cf838a2d58a2da91408ac8552a44d38b8
Marcus Williams <tue79528@tech-a007.tu.temple.edu>
2017-12-14 06:01:26 -0500

    Added basic GPS function back into the app. Next step will be to convert the lat and long values to human friendly region names..... but its 6 am.... so... i'm gonna take a nap... or get coffee.. sleep is for the weak

--- iCitizen/RootViewController.swift
+++ iCitizen/RootViewController.swift
@@ -7,35 +7,56 @@
 //
 
 import UIKit
+import CoreLocation
 
-class RootViewController: UIViewController, UIPageViewControllerDelegate {
+class RootViewController: UIViewController, UIPageViewControllerDelegate, CLLocationManagerDelegate {
 
     
     var pageViewController: UIPageViewController?
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var background: UIImageView!
     @IBOutlet weak var drive: UIButton!
     @IBOutlet weak var walk: UIButton!
     @IBOutlet weak var house: UIButton!
-        
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        locationLabel.text = String(locValue.latitude) + " " + String(locValue.longitude)
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Configure the page view controller and add it as a child view controller.
        drive.clipsToBounds = true
        drive.layer.cornerRadius = 50
-        walk.clipsToBounds = true
-        walk.layer.cornerRadius = 50
-        house.clipsToBounds = true
-        house.layer.cornerRadius = 50
+       walk.clipsToBounds = true
+       walk.layer.cornerRadius = 50
+       house.clipsToBounds = true
+       house.layer.cornerRadius = 50
+        
+    
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
 
